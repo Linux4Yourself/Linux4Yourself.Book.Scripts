903f885f214194ee181fb9cbf0435c8055886776
makpif <81463676+makpif@users.noreply.github.com>
2021-04-22 21:56:02 +0300

    Update file-system.sh
    мультилиб нужен не всем поэтому его вынести отсюда надо

--- src/file-system.sh
+++ src/file-system.sh
@@ -4,7 +4,7 @@ mkdir -pv $LIN/{boot,home,mnt,opt,srv}
 mkdir -pv $LIN/etc/{opt,sysconfig}
 mkdir -pv $LIN/lib/firmware
 mkdir -pv $LIN/media/{floppy,cdrom}
-mkdir -pv $LIN/usr/{,local/}{bin,include,lib,sbin,src,lib32}
+mkdir -pv $LIN/usr/{,local/}{bin,include,lib,sbin,src}
 mkdir -pv $LIN/usr/{,local/}share/{color,dict,doc,info,locale,man}
 mkdir -pv $LIN/usr/{,local/}share/{misc,terminfo,zoneinfo}
 mkdir -pv $LIN/usr/{,local/}share/man/man{1..8}
@@ -14,7 +14,6 @@ mkdir -pv $LIN/var/lib/{color,misc,locate}
 ln -sfv usr/bin $LIN/bin
 ln -sfv usr/lib $LIN/lib
 ln -sfv usr/sbin $LIN/sbin
-ln -sfv usr/lib32 $LIN/lib32
 
 ln -sfv /run $LIN/var/run
 ln -sfv /run/lock $LIN/var/lock
