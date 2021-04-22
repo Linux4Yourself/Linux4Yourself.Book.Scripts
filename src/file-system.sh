1766a673202e361af164fab6758ad99c964c7e7d
makpif <81463676+makpif@users.noreply.github.com>
2021-04-22 21:55:22 +0300

    Update file-system.sh
    думаю запускать этот скрипт не из чрута, а в самом начале

--- src/file-system.sh
+++ src/file-system.sh
@@ -1,23 +1,23 @@
-mkdir -pv /{usr,etc,var}
-mkdir -pv /{boot,home,mnt,opt,srv}
+mkdir -pv $LIN/{usr,etc,var}
+mkdir -pv $LIN/{boot,home,mnt,opt,srv}
 
-mkdir -pv /etc/{opt,sysconfig}
-mkdir -pv /lib/firmware
-mkdir -pv /media/{floppy,cdrom}
-mkdir -pv /usr/{,local/}{bin,include,lib,sbin,src,lib32}
-mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
-mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
-mkdir -pv /usr/{,local/}share/man/man{1..8}
-mkdir -pv /var/{cache,local,log,mail,opt,spool}
-mkdir -pv /var/lib/{color,misc,locate}
+mkdir -pv $LIN/etc/{opt,sysconfig}
+mkdir -pv $LIN/lib/firmware
+mkdir -pv $LIN/media/{floppy,cdrom}
+mkdir -pv $LIN/usr/{,local/}{bin,include,lib,sbin,src,lib32}
+mkdir -pv $LIN/usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -pv $LIN/usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -pv $LIN/usr/{,local/}share/man/man{1..8}
+mkdir -pv $LIN/var/{cache,local,log,mail,opt,spool}
+mkdir -pv $LIN/var/lib/{color,misc,locate}
 
-ln -sfv usr/bin /bin
-ln -sfv usr/lib /lib
-ln -sfv usr/sbin /sbin
-ln -sfv usr/lib32 /lib32
+ln -sfv usr/bin $LIN/bin
+ln -sfv usr/lib $LIN/lib
+ln -sfv usr/sbin $LIN/sbin
+ln -sfv usr/lib32 $LIN/lib32
 
-ln -sfv /run /var/run
-ln -sfv /run/lock /var/lock
+ln -sfv /run $LIN/var/run
+ln -sfv /run/lock $LIN/var/lock
 
-install -dv -m 0750 /root
-install -dv -m 1777 /tmp /var/tmp
+install -dv -m 0750 $LIN/root
+install -dv -m 1777 $LIN/tmp $LIN/var/tmp
