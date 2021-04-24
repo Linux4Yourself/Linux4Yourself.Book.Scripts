1774c829a924a4cc1986dba8db2f01cf26e659f1
makpif <81463676+makpif@users.noreply.github.com>
2021-04-24 09:27:16 +0300

    исправил создание симлинков на юзр
    исправил ошибку

--- src/file-system.sh
+++ src/file-system.sh
@@ -1,6 +1,10 @@
 mkdir -pv $LIN/{usr,etc,var}
 mkdir -pv $LIN/{boot,home,mnt,opt,srv}
 
+ln -sfv usr/bin $LIN/bin
+ln -sfv usr/lib $LIN/lib
+ln -sfv usr/sbin $LIN/sbin
+
 mkdir -pv $LIN/etc/{opt,sysconfig}
 mkdir -pv $LIN/lib/firmware
 mkdir -pv $LIN/media/{floppy,cdrom}
@@ -10,10 +14,6 @@ mkdir -pv $LIN/usr/{,local/}share/{misc,terminfo,zoneinfo}
 mkdir -pv $LIN/usr/{,local/}share/man/man{1..8}
 mkdir -pv $LIN/var/{cache,local,log,mail,opt,spool}
 mkdir -pv $LIN/var/lib/{color,misc,locate}
-
-ln -sfv usr/bin $LIN/bin
-ln -sfv usr/lib $LIN/lib
-ln -sfv usr/sbin $LIN/sbin
 
 ln -sfv /run $LIN/var/run
 ln -sfv /run/lock $LIN/var/lock
