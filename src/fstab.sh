b16b4f6b0683d82d5c24aca0e8628fafd1291bd7
makpif <citokot@gmail.com>
2021-06-02 09:47:14 +0300

    Fix fstab

--- src/fstab.sh
+++ src/fstab.sh
@@ -1,13 +1,7 @@
 cat > /etc/fstab << "EOF"
-# Begin /etc/fstab
-
 # file system  mount-point  type     options             dump  fsck
 #                                                              order
 
 
 /dev/sdX     /              ext4    defaults             1     1 
-/dev/sdN     /boot/efi      vfat    umask=0077           0     0
-/dev/sdY     swap           swap     pri=1               0     0
-
-# End /etc/fstab
 EOF
